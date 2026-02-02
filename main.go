d52d39cd75caf1207f6d81e213764c8535bf8eab
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-02-02 17:56:25 +0100

    main

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/b0bbywan/go-odio-api/api"
 	"github.com/b0bbywan/go-odio-api/config"
@@ -32,6 +36,35 @@ func main() {
 	mux := http.NewServeMux()
 	api.Register(mux, pa, sd)
 
+	// HTTP server
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// Goroutine pour signal handling
+
+	// Goroutine pour signal handling
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+		<-sigChan
+
+		log.Println("Shutdown signal received, stopping server...")
+
+		// Arrêter le listener D-Bus d'abord
+		sd.Close()
+		pa.Close()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("Server shutdown error: %v", err)
+		}
+	}()
+
 	log.Println("Odio Audio API running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Printf("Server error: %v", err)
+	}
 }
